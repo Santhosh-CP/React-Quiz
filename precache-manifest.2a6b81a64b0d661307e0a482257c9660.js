7bdb434eceedaf8d0a2f06b09da3b3b21e82b796
ShupperMuppet <pok.santhosh@gmail.com>
2020-07-17 20:07:45 +0530

    Updates

---
+++ precache-manifest.2a6b81a64b0d661307e0a482257c9660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ded3af8f69ec13c977ba7542b1d105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "947db3ae122181793ad5",
+    "url": "/static/js/2.106d0316.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.106d0316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d3de9a8c8c15eaf6ec",
+    "url": "/static/js/main.ecfbc2cd.chunk.js"
+  },
+  {
+    "revision": "0cead240beaeac72778b",
+    "url": "/static/js/runtime-main.50b5716e.js"
+  },
+  {
+    "revision": "80c0cc9d2cff60104b544eedbd9a1f5a",
+    "url": "/static/media/harrison-hargrave-46M8KAnUBlI-unsplash.80c0cc9d.jpg"
+  }
+]);
